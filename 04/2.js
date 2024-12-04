cb28b98044fe7cece119cb1fd61933e93cbe9f64
Rick Runyon <rick.runyon@gmail.com>
2024-12-04 10:04:16 -0500

    4/2

--- 04/2.js
+++ 04/2.js
@@ -1,31 +1,18 @@
 import * as fs from 'fs';
 
-/*
-Split on "don't()" and "do()"
-
-Split on don't first, then split each segment again on do, and ignore the first segment of each subsegment
-*/
 function solution() {
-  let input = fs.readFileSync('./03/input.txt', { encoding: 'utf8', flag: 'r' });
-
-  let split = input.split("don't()");
-  split = split.map(segment => segment.split("do()"));
-
-  const pattern = /mul\((\d+),(\d+)\)/g;
+  let input = fs.readFileSync('./04/input.txt', { encoding: 'utf8', flag: 'r' }).split('\n');
 
   let total = 0;
-  for (let i = 0; i < split.length; i++) {
-    let segment = split[i];
 
-    for (let j = 0; j < segment.length; j++) {
-      // Leftmost subsegment of each segment is don't, except for the first segment
-      if (i !== 0 && j === 0) continue;
-
-      let subsegment = segment[j];
-      const matches = Array.from(subsegment.matchAll(pattern));
-
-      for (let [, left, right] of matches) {
-        total += Number(left) * Number(right);
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      if (input[i][j] === 'A') {
+        try {
+          const forwardDiagonal = [input[i-1][j+1], input[i][j], input[i+1][j-1]].join('');
+          const backwardDiagonal = [input[i-1][j-1], input[i][j], input[i+1][j+1]].join('');
+          if ((forwardDiagonal === 'MAS' || forwardDiagonal === 'SAM') && (backwardDiagonal === 'MAS' || backwardDiagonal === 'SAM')) total++;
+        } catch {}
       }
     }
   }
