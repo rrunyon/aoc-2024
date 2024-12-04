b1a7502051b616dfe7a279d5c53e9617d05664fe
Rick Runyon <rick.runyon@gmail.com>
2024-12-03 19:07:25 -0500

    3/2

--- 03/2.js
+++ 03/2.js
@@ -1,7 +1,47 @@
 import * as fs from 'fs';
 
+/*
+Split on "don't()" and "do()"
+
+Split on don't first, then split each segment again on do, and ignore the first segment of each subsegment
+*/
 function solution() {
-  let input = fs.readFileSync('./03/input.txt', { encoding: 'utf8', flag: 'r' }).split('\n');
+  let input = fs.readFileSync('./03/input.txt', { encoding: 'utf8', flag: 'r' });
+
+  let split = input.split("don't()");
+  split = split.map(segment => segment.split("do()"));
+
+  const pattern = /mul\((\d+),(\d+)\)/g;
+
+  let total = 0;
+  for (let i = 0; i < split.length; i++) {
+    let segment = split[i];
+
+    // left most segment is do
+    if (i === 0) {
+      for (let subsegment of segment) {
+
+        const matches = Array.from(subsegment.matchAll(pattern));
+
+        for (let [, left, right] of matches) {
+          total += Number(left) * Number(right);
+        }
+      }
+    } else {
+      // Leftmost subsegment of each segment is don't
+      for (let j = 1; j < segment.length; j++) {
+        let subsegment = segment[j];
+        const matches = Array.from(subsegment.matchAll(pattern));
+
+        for (let [, left, right] of matches) {
+          total += Number(left) * Number(right);
+        }
+
+      }
+    }
+  }
+
+  return total;
 }
 
 console.log(solution());
